580b7d2930634d8e648bec8b41e8bc479915d1c3
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-08-25 10:45:02 -0700

    excercise with two sorted arrays

---
+++ 07_2025/twoSumSortedArray.ts
@@ -0,0 +1,21 @@
+function twoSumDos(numbers: number[], target: number): number[] {
+    let left = 0;
+    let right = numbers.length - 1;
+
+    while (left < right)
+    {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target)
+        {
+            return [left+1, right+1];
+        }
+        if (sum > target)
+        {
+            right--;
+        } else
+        {
+            left++;
+        }
+    }
+    return [];
+};
