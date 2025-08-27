eb87f66fc85e75c4bba8ce67c18f9a95698fa00f
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-08-27 10:13:07 -0700

    Reverse integer (32 bits)

---
+++ 08_2025/reverseInteger.ts
@@ -0,0 +1,19 @@
+function reverse(x: number): number {
+    let reversed = 0;
+    let sign = x < 0 ? -1 : 1;
+    x = Math.abs(x);
+
+    while (x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    reversed *= sign;
+
+    // Check for 32-bit integer overflow
+    if (reversed < Math.pow(-2, 31) || reversed > Math.pow(2, 31) - 1) {
+        return 0;
+    }
+
+    return reversed;
+};
