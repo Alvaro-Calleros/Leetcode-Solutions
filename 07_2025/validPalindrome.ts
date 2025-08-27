47b3711b787d5502802a5256b1d549e904c57e22
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-08-27 10:11:02 -0700

    Valid palindrome (classic)

---
+++ 07_2025/validPalindrome.ts
@@ -0,0 +1,16 @@
+
+function isPalindrome(s: string): boolean {
+    
+    const cleaned = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+    for (let i = 0; i < cleaned.length / 2; i++)
+    {
+
+        if (cleaned[i] != cleaned[cleaned.length - 1 - i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+};
