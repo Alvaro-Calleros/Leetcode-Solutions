585d60f324e2de8525b4c30fa3ca0d699a5d0442
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-01 11:03:16 -0700

    From the NeetCode 150 plan; contains duplcate in javascript

---
+++ 09_2025/containsDuplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = {};
+    for (let i = 0; i < nums.length; i++) {
+        
+        if (map.hasOwnProperty(nums[i])) {
+            return true; 
+        }
+        map[nums[i]] = i;
+    }
+    return false;
+};
