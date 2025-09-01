6046ee732a2329a4053ad831b22b8c52b82547a3
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-01 11:01:41 -0700

    2 pointer for container with most water

---
+++ 08_2025/ContainerWtMostWater.ts
@@ -0,0 +1,10 @@
+function maxArea(height: number[]): number {
+    let max = 0, left = 0, right = height.length - 1;
+    while (left < right) {
+        const area = Math.min(height[left], height[right]) * (right - left);
+        if (area > max) max = area;
+        if (height[left] < height[right]) left++;
+        else right--;
+    }
+    return max;
+};
