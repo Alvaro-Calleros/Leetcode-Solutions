03755d3bc81dc60945a0ee11f13cd49e8e9befd9
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-02 09:07:42 -0700

    descriptive comments added

--- 09_2025/containsDuplicate.js
+++ 09_2025/containsDuplicate.js
@@ -3,13 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const map = {};
+    const map = {}; // we'll map each number to its index
     for (let i = 0; i < nums.length; i++) {
         
-        if (map.hasOwnProperty(nums[i])) {
-            return true; 
+        if (map.hasOwnProperty(nums[i])) { // If anytime we find a duplicate
+            return true; // we return true
         }
         map[nums[i]] = i;
     }
-    return false;
+    return false; // if We did not find duplicate, return false
 };
