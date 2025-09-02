8dd4b11b206f0a9dbe1524e47010a7166ef6f8ea
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-02 09:07:50 -0700

    descriptive comments added

--- 09_2025/validAnagram.js
+++ 09_2025/validAnagram.js
@@ -5,22 +5,22 @@
  */
 var isAnagram = function(s, t) {
     {
-        if (s.length !== t.length) return false;
+        if (s.length !== t.length) return false; //if the lengths are not equal, they cannot be anagrams
 
-        const cS = {};
-        const cT = {};
+        const cS = {}; // Counter for string s
+        const cT = {}; // Counter for string t
 
-        for (let i = 0; i < s.length; i++)
+        for (let i = 0; i < s.length; i++) //we map each character to its frequency
         {
             cS[s[i]] = (cS[s[i]] || 0) + 1;
             cT[t[i]] = (cT[t[i]] || 0) + 1;
         }
 
-        for (const j in cS)
+        for (const j in cS) // we compare the frequency maps
         {
-            if (cS[j] !== cT[j]) return false;
+            if (cS[j] !== cT[j]) return false; // if any character has different frequencies, they are not anagrams
         }
-        return true;
+        return true; // if we didn't find any differences, they are anagrams
     }
 
 };
