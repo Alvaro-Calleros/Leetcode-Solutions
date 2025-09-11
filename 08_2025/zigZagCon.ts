5b399e675046841e775966d65ec42db538e9223d
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-11 13:54:46 -0700

    Descriptive comments

--- 08_2025/zigZagCon.ts
+++ 08_2025/zigZagCon.ts
@@ -1,19 +1,21 @@
 function convert(s: string, numRows: number): string {
-    if(numRows === 1)  return s;
 
-    let direction = false;
+    // Instead of a zigzag we think of the rows as separate strings and a staircase pattern
+    if(numRows === 1)  return s; // If there's only one row, return the original string
+
+    let direction = false; // for the staircase pattern we use a pointer direction as a boolean
     let count = 0;
 
-    let arr = new Array(numRows).fill("");
+    let arr = new Array(numRows).fill(""); // Create an array to hold the rows
 
-    for(let i = 0; i<s.length; i++)
+    for(let i = 0; i<s.length; i++) //for loop that iterates through each character in the string
     {
-        let curr = s[i];
-        arr[count] += curr;
+        let curr = s[i]; // Get the current character
+        arr[count] += curr; // Append the current character to the current row
 
-        if (count === 0 || count >= numRows-1) direction = !direction;
+        if (count === 0 || count >= numRows-1) direction = !direction; // Change direction if we hit the top or bottom
 
-        direction ? count++: count--;
+        direction ? count++: count--; // Move the pointer up or down
     }
-    return arr.join("");
+    return arr.join(""); // At the end we concatenate all rows
 };
