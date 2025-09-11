7ac7cb870e310c15e728b671b5e876d20be5afee
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-11 13:54:25 -0700

    Descriptive comments

--- 08_2025/sudokuSolver.ts
+++ 08_2025/sudokuSolver.ts
@@ -3,34 +3,42 @@
  */
 function solveSudoku(board: string[][]): void {
     const isValid = (row: number, col: number, num: string): boolean => {
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < 9; i++)
+            { // Check the row, column, and 3x3 subgrid
             if (board[row][i] === num || board[i][col] === num || board[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + (i % 3)] === num) {
-                return false;
+                return false; // if we found a conflict, the number is not valid
             }
         }
-        return true;
+        return true; // if we didn't find any conflicts, the number is valid
     };
 
-    const solve = (): boolean => {
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 9; j++) {
-                if (board[i][j] === '.') {
-                    for (let num = 1; num <= 9; num++) {
+    const solve = (): boolean => { // Backtracking function
+        for (let i = 0; i < 9; i++) 
+        { 
+            for (let j = 0; j < 9; j++)
+            {
+                if (board[i][j] === '.')
+                {
+                    for (let num = 1; num <= 9; num++)
+                    {
                         const n = num.toString();
-                        if (isValid(i, j, n)) {
+
+                        if (isValid(i, j, n)) // call isValid using the current row, column, and number
+                        {
                             board[i][j] = n;
-                            if (solve()) {
-                                return true;
+                            if (solve())
+                            {
+                                return true; // if we found a solution, return true
                             }
                             board[i][j] = '.';
                         }
                     }
-                    return false;
+                    return false; // if we didn't find a solution, return false
                 }
             }
         }
-        return true;
+        return true; // if we filled all cells successfully, the Sudoku is solved
     };
 
-    solve();
+    solve(); // Let's solve the Sudoku puzzle
 };
