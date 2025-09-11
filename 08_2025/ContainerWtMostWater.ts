795ffa64bb6988fc9426eb05a7bd4b3f374ef087
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-11 13:54:16 -0700

    Descriptive comments

--- 08_2025/ContainerWtMostWater.ts
+++ 08_2025/ContainerWtMostWater.ts
@@ -1,10 +1,12 @@
 function maxArea(height: number[]): number {
-    let max = 0, left = 0, right = height.length - 1;
-    while (left < right) {
+
+    let max = 0, left = 0, right = height.length - 1; // we use 2 pointers to find the maximum area
+
+    while (left < right) { // we iterate until the left pointer is less than the right pointer
         const area = Math.min(height[left], height[right]) * (right - left);
-        if (area > max) max = area;
+        if (area > max) max = area; // each time we find a larger area, we update max
         if (height[left] < height[right]) left++;
         else right--;
     }
-    return max;
+    return max; //after looping through all possible areas, we return the maximum found
 };
